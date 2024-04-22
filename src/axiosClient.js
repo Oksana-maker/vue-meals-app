86f0959abfc4e5cc6b4307bc44ac5c0c762ee153
Oksana-maker <oksana.mashurova@gmail.com>
2024-04-22 17:42:50 +0200

    first

--- src/axiosClient.js
+++ src/axiosClient.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
-});
 
+  baseURL: 'https://www.themealdb.com/api/json/v1/1/'
+});
+console.log('hier')
 
 export default axiosClient;
